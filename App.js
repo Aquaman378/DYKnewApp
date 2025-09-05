7c757b6a915cf7debfc46f1f7658ae0585138627
Aquaman378 <jaonalexh37@yahoo.com>
2025-09-05 13:29:05 -1000

    Updated 9/5

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, Button, Alert, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, Alert, SafeAreaView, Seperator, FlatList } from 'react-native';
 import { useState } from 'react';
 
 //Will be a todo list but change name of app
-
+const initialData = [
+  
+];
 
 export default function App() {
-  
+  const [data, setData] = useState(initialData);
+  const [inputValue, setInputValue] = useState('');
+
+  const addToList = () => {
+    if (inputValue.trim() === '') return;
+    setData([...data, { id: (data.length + 1), title: inputValue }]);
+    setInputValue('');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style = {styles.title}>Welcome to Your To Do List</Text>
-
+      <Text style={styles.title}>Welcome to Your To Do List</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Type here"
+        value={inputValue}
+        onChangeText={setInputValue}
+      />
       <StatusBar style="auto" />
+
+      <Button
+        title="Add to List"
+        onPress={addToList}
+      />
+
+      <FlatList
+        style={{ marginTop: 20, width: '100%', alignContent: 'flex-end', borderColor: 'black' }}
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Text>{item.title}</Text>
+        )}
+      />
     </SafeAreaView>
   );
 }
